63b0407cbabc81cdab93296463b825ed1d70b4ce
mahafujriyan <mahafujhossainriyan02@gmail.com>
2026-06-05 00:36:08 +0600

    Adde the solve of deployment issue

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,6 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "../src/app";
 
-export default function handler(
-  req: VercelRequest,
-  res: VercelResponse
-): Promise<void> {
-  return new Promise((resolve, reject) => {
-    app(req, res, (err: unknown) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
-    });
-  });
-}
+export default app;
 
 export const config = {
   maxDuration: 30,
